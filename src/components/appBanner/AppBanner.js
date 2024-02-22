7ce1772662d0a9953f4fd030251c75732c682743
Timur <Stafr95@mail.ru>
2024-02-22 17:57:18 +0500

    Creating an AppBanner Component

---
+++ src/components/appBanner/AppBanner.js
@@ -0,0 +1,18 @@
+import avengers from '../../resources/img/Avengers.png';
+import avengersLogo from '../../resources/img/Avengers_logo.png.png';
+
+import('./appBanner.scss')
+
+const AppBanner = () => {
+	return (
+		<div className='app__banner'>
+			<img src={avengers} alt="Avengers"/>
+			<div className='app__banner-text'>
+				New comics every week! <br/>
+				Stay tuned!
+			</div>
+			<img src={avengersLogo} alt="Avengers Logo"/>
+		</div>
+	)
+}
+export default AppBanner;
