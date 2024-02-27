fa6b0b4e5deb93e0144c364ef7b4dbeaacfdbcb0
Timur <Stafr95@mail.ru>
2024-02-27 12:35:11 +0500

    Inline styling

--- src/components/errorMesagge/ErrorMessage.js
+++ src/components/errorMesagge/ErrorMessage.js
@@ -1,7 +1,7 @@
-import img from './error.mp4';
+import img from './error.gif';
 const ErrorMessage = () => {
 	return (
-		<img style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}} src={img} alt={error} />
+		<img style={{display: 'block', width: '450px', height: '250px', objectFit: 'contain', margin: '0 auto'}} src={img} alt='Error' />
 	)
 }
 
