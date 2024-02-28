fd497f582e8134bf16e052f1b70cc3d1090ed4d7
Timur <Stafr95@mail.ru>
2024-02-28 15:48:44 +0500

    Getting a Character ID

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,6 +7,17 @@ import CharInfo from '../charInfo/CharInfo';
 import decoration from '../../resources/img/vision.png';
 
 class App extends Component {
+
+	state = {
+		selectedChar: null
+	}
+
+	onCharSelected = (id) => {
+		this.setState({
+			selectedChar: id
+		})
+	}
+
 	render() {
 		return (
 			<div className="app">
@@ -14,10 +25,10 @@ class App extends Component {
 				<main>
 					<RandomChar />
 					<div className="char__content">
-						<CharList />
-						<CharInfo />
+						<CharList onCharSelected={this.onCharSelected} />
+						<CharInfo charId={this.state.selectedChar} />
 					</div>
-					<img className="bg-decoration" src="{decoration}" alt="vision"/>
+					<img className="bg-decoration" src={decoration} alt="vision"/>
 				</main>
 			</div>
 		)
