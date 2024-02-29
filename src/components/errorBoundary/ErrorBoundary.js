e2b950233b812cfbc3004a4b4ea38d9d4c16928a
Timur <Stafr95@mail.ru>
2024-02-29 14:02:31 +0500

    Creating a class component ErrorBoundary

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import {Component} from "react";
+import ErrorMessage from "../errorMesagge/ErrorMessage";
+
+class ErrorBoundary extends Component {
+	state = {
+		error: false
+	}
+
+
+	componentDidCatch(error, errorInfo) {
+		console.log(error, errorInfo);
+		this.setState({
+			error: true
+		})
+	}
+
+	render() {
+		if (this.state.error) {
+			// return <h2 style={{margin: '50% auto 0 auto', fontSize: '40px', color: 'red'}}>Something went wrong</h2>
+			return <ErrorMessage />
+		}
+
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundary;
