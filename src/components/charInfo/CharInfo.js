cf04700a279fec6c244b88030dea87b5d0ee2d93
Timur <Stafr95@mail.ru>
2024-02-29 14:02:46 +0500

    Checking for fuse errors

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -27,6 +27,7 @@ class CharInfo extends Component {
         }
     }
 
+
     updateChar = () => {
         const {charId} = this.props;
         if (!charId) {
@@ -40,6 +41,8 @@ class CharInfo extends Component {
             .then(this.onCharLoaded)
             .catch(this.onError)
     }
+
+
 
     onCharLoaded = (char) => {
         this.setState({
