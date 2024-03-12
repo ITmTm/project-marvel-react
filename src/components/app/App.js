16f16a1ef7518b1a5806a1ef7516a4b1130bd807
Timur <Stafr95@mail.ru>
2024-03-12 22:03:35 +0500

    Working and installing React Router v5

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,14 @@
 // import {Component} from "react";
 
 import {useState} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
 import AppHeader from '../appHeader/AppHeader';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
@@ -22,23 +26,27 @@ const App = () => {
 	}
 
 	return (
-		<div className="app">
-			<AppHeader />
-			<main>
-				<ErrorBoundary>
-					<RandomChar />
-				</ErrorBoundary>
-				<div className="char__content">
-					<ErrorBoundary>
-						<CharList onCharSelected={onCharSelected} />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<CharInfo charId={selectedChar} />
-					</ErrorBoundary>
-				</div>
-				<img className="bg-decoration" src={decoration} alt="vision"/>
-			</main>
-		</div>
+		<Router>
+			<div className="app">
+				<AppHeader/>
+				<main>
+					{/*<ErrorBoundary>*/}
+					{/*	<RandomChar />*/}
+					{/*</ErrorBoundary>*/}
+					{/*<div className="char__content">*/}
+					{/*	<ErrorBoundary>*/}
+					{/*		<CharList onCharSelected={onCharSelected} />*/}
+					{/*	</ErrorBoundary>*/}
+					{/*	<ErrorBoundary>*/}
+					{/*		<CharInfo charId={selectedChar} />*/}
+					{/*	</ErrorBoundary>*/}
+					{/*</div>*/}
+					{/*<img className="bg-decoration" src={decoration} alt="vision"/>*/}
+					<AppBanner/>
+					<ComicsList/>
+				</main>
+			</div>
+		</Router>
 	)
 }
 
