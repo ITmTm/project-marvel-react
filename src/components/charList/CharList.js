0a3b56b27503acd732e2cc0b0288a2e511d44f47
Timur <Stafr95@mail.ru>
2024-04-20 19:17:35 +0500

    Adding smooth appearance of items

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,10 +1,12 @@
 // import {Component} from "react";
 import {useState, useEffect, useRef} from "react";
 import PropTypes from "prop-types";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
+import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import useMarvelService from "../../services/MarvelService";
+
 import './charList.scss';
 
 
@@ -65,42 +67,45 @@ const CharList = (props) => {
             }
 
             return (
-                <li
-                    className="char__item"
-                    tabIndex={0}
-                    ref={el => itemRefs.current[i] = el}
-                    key={item.id}
-                    onClick={() => {
-                        props.onCharSelected(item.id);
-                        focusOnItem(i);
-                    }}
-                    onKeyDown={(e) => {
-                        if (e.key === '' || e.key === 'Enter') {
+                <CSSTransition key={item.id} timeout={500} classNames='char__item'>
+                    <li
+                        className="char__item"
+                        tabIndex={0}
+                        ref={el => itemRefs.current[i] = el}
+                        key={item.id}
+                        onClick={() => {
                             props.onCharSelected(item.id);
-                            focusOnItem(i)
-                        }
-                    }}
-                >
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                    <div className="char__name">{item.name}</div>
-                </li>
+                            focusOnItem(i);
+                        }}
+                        onKeyDown={(e) => {
+                            if (e.key === '' || e.key === 'Enter') {
+                                props.onCharSelected(item.id);
+                                focusOnItem(i)
+                            }
+                        }}
+                    >
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                    </li>
+                </CSSTransition>
             )
         });
 
         // Эта конструкция вынесена для центровки спиннера/ошибкиж
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
 
 
-
     const items = renderItems(charList);
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading && !newItemLoading ? <Spinner /> : null;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading && !newItemLoading ? <Spinner/> : null;
 
     return (
         <div className="char__list">
