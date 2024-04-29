cffa46ba59a3c248e6bca4210e3029ab5e9259b4
Timur <Stafr95@mail.ru>
2024-04-29 20:26:11 +0500

    Removing the CSSTransition library

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,6 @@
 // import {Component} from "react";
 import {lazy, Suspense} from "react";
 import {BrowserRouter as Router, Route, Routes, useLocation} from "react-router-dom";
-import {CSSTransition, SwitchTransition} from "react-transition-group";
 
 import AppHeader from '../appHeader/AppHeader';
 import Spinner from "../spinner/Spinner";
@@ -20,16 +19,12 @@ const MainContent = () => {
 	const location = useLocation();
 
 	return (
-		<SwitchTransition>
-			<CSSTransition key={location.key} classNames='fade-page' timeout={700} unmountOnExit>
-				<Routes location={location}>
-					<Route path='/' element={<MainPage />} />
-					<Route path='/comics' element={<ComicsPage />} />
-					<Route path='/comics/:comicId' element={<SingleComicPage />} />
-					<Route path='*' element={<Page404 />} />
-				</Routes>
-			</CSSTransition>
-		</SwitchTransition>
+		<Routes location={location}>
+			<Route path='/' element={<MainPage />} />
+			<Route path='/comics' element={<ComicsPage />} />
+			<Route path='/comics/:comicId' element={<SingleComicPage />} />
+			<Route path='*' element={<Page404 />} />
+		</Routes>
 	)
 }
 
