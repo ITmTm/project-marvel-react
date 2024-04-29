d60166357832476bc909773c4ceac3dba272e99f
Timur <Stafr95@mail.ru>
2024-04-29 19:27:17 +0500

    Application of FSM (finite-state machine)

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,7 +9,25 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 
 import './charList.scss';
 
+const setContent = (process, Component, newItemLoading) => {
+	switch (process) {
+		case 'waiting':
+			return <Spinner />;
+			break;
+		case 'loading':
+			return newItemLoading ? <Component /> : <Spinner />;
+			break;
+		case 'confirmed':
+			return <Component />;
+			break;
+		case 'error':
+			return <ErrorMessage />;
+			break;
 
+		default:
+			throw new Error('Unexpected process state');
+	}
+}
                                 // Функциональный компонент hook(i)
 
 const CharList = (props) => {
@@ -21,7 +39,7 @@ const CharList = (props) => {
 	const [animStart, setAnimStart] = useState(false);
 
 
-    const {loading, error, getAllCharacters} = useMarvelService();
+    const {loading, error, getAllCharacters, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         onRequest(offset, true);
@@ -32,7 +50,8 @@ const CharList = (props) => {
     const onRequest = (offset, initial) => {
         initial ? setNewItemLoading(false) : setNewItemLoading(true);
         getAllCharacters(offset)
-            .then(onCharListLoaded);
+            .then(onCharListLoaded)
+			.then(() => setProcess('confirmed'))
     }
 
 
@@ -136,16 +155,9 @@ const CharList = (props) => {
     }
 
 
-    const items = renderItems(charList);
-
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading && !newItemLoading ? <Spinner/> : null;
-
     return (
         <div className="char__list">
-            {errorMessage}
-            {spinner}
-            {items}
+			{setContent(process, () => renderItems(charList), newItemLoading)}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
